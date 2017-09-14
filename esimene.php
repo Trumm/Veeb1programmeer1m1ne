bf80d451627bfe79d3d9be0c1c953a5b0d4a0be8
Edgar Johannes Trumm <trumm@users.noreply.github.com>
2017-09-14 10:46:52 +0300

    Lisatud on esimene rida php koodi

---
+++ esimene.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <title>Edgar Johannes Trumm</title>
+</head>
+ <body>
+   <h1>Edgar Johannes Trumm</h1>
+   <p>Ei sisalda mingisugust tõsiseltvõetavat sisu.</p>
+   <?php
+echo "Algas PHP õppimine";
+   ?>
+   
+   
+ </body>
+</html>
