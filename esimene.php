a74cb6e110a4d720d36d5e066b953a505f854bf4
Edgar Johannes Trumm <trumm@users.noreply.github.com>
2017-09-14 11:31:17 +0300

    lisatud muutujad ning IF lause

--- esimene.php
+++ esimene.php
@@ -1,3 +1,16 @@
+<?php
+//muutujad
+$myName = "Edgar Johannes";
+$myFamilyName = "Trumm";
+//hindan päeva osa võrdlemine ( > < >= <= == != )
+$hourNow = date("H");
+$partofDay = "";
+if ($hourNow < 8){$partofDay = "varajane hommik";}
+if ($hourNow >= 8 and $hourNow < 16){$partofDay = "koolipäev";}
+if ($hourNow >16){$partofDay = "vaba aeg";}
+//echo $partofDay
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,10 +18,13 @@
    <title>Edgar Johannes Trumm</title>
 </head>
  <body>
-   <h1>Edgar Johannes Trumm</h1>
+   <h1><?php echo $myName ." " .$myFamilyName; ?></h1>
    <p>Ei sisalda mingisugust tõsiseltvõetavat sisu.</p>
    <?php
-echo "Algas PHP õppimine";
+echo "<p>Algas PHP õppimine</p>";
+echo "<p>Täna on ";
+echo date("d.m.Y") .", kell oli lehe avamise hetkel " .date("H:i:s");
+echo ", hetkel on" .$partofDay .".</p>";
    ?>
    
    
