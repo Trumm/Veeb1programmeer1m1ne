6505d2b445b6ae1f1a6e7884edb2e7fad7d70880
Edgar Johannes Trumm <trumm@users.noreply.github.com>
2017-09-21 10:24:07 +0300

    Lisatud massiiv, for tsükkel ja esimene vorm

--- esimene.php
+++ esimene.php
@@ -2,6 +2,11 @@
 //muutujad
 $myName = "Edgar Johannes";
 $myFamilyName = "Trumm";
+$monthNamesEt=["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august",
+"september", "oktoober", "november", "detsember"];
+//var_dump($monthNamesEt);
+//echo $monthNamesEt [3];
+$monthNow=$monthNamesEt[date("n")-1];
 //hindan pÃ¤eva osa vÃµrdlemine ( > < >= <= == != )
 $hourNow = date("H");
 $partofDay = "";
@@ -9,6 +14,23 @@ if ($hourNow < 8){$partofDay = "varajane hommik";}
 if ($hourNow >= 8 and $hourNow < 16){$partofDay = "koolipÃ¤ev";}
 if ($hourNow >16){$partofDay = "vaba aeg";}
 //echo $partofDay
+//vanusega tegelemine
+//var_dump($_POST);
+//echo $_POST["birthYear"];
+$mybirthYear;
+if ( isset($_POST["birthYear"]) and $_POST["birthYear"] !=0){
+	$myAge = date("Y") - $_POST["birthYear"];
+	$mybirthYear = $_POST["birthYear"];
+	$ageNotice = "<p>Te olete umbkaudu " .$myAge ." aastat vana.</p>";
+	$ageNotice .= "<p>Olete elanud järgnevatel aastatel:</p> <ol>";
+	for ($i = $mybirthYear; $i <= date("Y"); $i++){
+	$ageNotice .="<li>" .$i ."</li>";
+	}
+	 $ageNotice .= "</ol>";
+	}
+	/*for ($i = 0; $i < 5; $i ++){
+		echo "ho";
+	}*/
 ?>
 
 <!DOCTYPE html>
@@ -23,10 +45,19 @@ if ($hourNow >16){$partofDay = "vaba aeg";}
    <?php
 echo "<p>Algas PHP Ãµppimine</p>";
 echo "<p>TÃ¤na on ";
-echo date("d.m.Y") .", kell oli lehe avamise hetkel " .date("H:i:s");
-echo ", hetkel on" .$partofDay .".</p>";
+echo date("d. ") .$monthNow .date(" Y") .", kell oli lehe avamise hetkel " .date("H:i:s");
+echo ", hetkel on " .$partofDay .".</p>";
    ?>
-   
-   
+	<h2>Natuke vanusest</h2>
+	<form method="POST">
+	<label>Teie sünniaasta</label>
+	<input name="birthYear" id="birthYear" type="number" value="<?php echo $mybirthYear; ?>" min="1900" max="2017">
+	<input name="sumbitbirthYear" type="submit" value="Sisesta">
+	</form>
+   <?php
+   if($ageNotice != ""){
+	   echo $ageNotice;
+   }
+   ?>
  </body>
 </html>
