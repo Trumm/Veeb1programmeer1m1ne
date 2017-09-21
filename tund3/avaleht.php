4eb9856870b108d629e6734c84296fbcb9132d56
Edgar Johannes Trumm <trumm@users.noreply.github.com>
2017-09-21 11:38:24 +0300

    Lisasime pilte

---
+++ tund3/avaleht.php
@@ -0,0 +1,21 @@
+<?php
+//muutujad
+$myName = "Edgar Johannes";
+$myFamilyName = "Trumm";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <title>Edgar Johannes Trumm</title>
+</head>
+ <body>
+   <h1><?php echo $myName ." " .$myFamilyName; ?></h1>
+   <p>Ei sisalda mingisugust t√µsiseltv√µetavat sisu.</p>
+ <p>Meie ıpime <a href="http://www.tlu.ee">Tallinna ‹likoolis</a>.</p>
+ <p>Minu esimene PHP leht on <a href="../esimene.php">siin</a></p>
+ <p>Minu sıbra Meelise leht on <a href="../../../~lutsmeel/Veebiprogrammeerimine">siin</a></p>
+ </body>
+</html>
